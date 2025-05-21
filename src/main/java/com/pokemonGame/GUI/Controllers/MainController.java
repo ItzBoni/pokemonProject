caca1bdcaa387de4bb6e577dad3dc7e94a49a82b
Santiago Bonilla <112128287+ItzBoni@users.noreply.github.com>
2025-05-21 12:05:15 -0600

    Fixed commit issue part 2.

--- src/main/java/com/pokemonGame/GUI/Controllers/MainController.java
+++ src/main/java/com/pokemonGame/GUI/Controllers/MainController.java
@@ -1,4 +1,33 @@
 package com.pokemonGame.GUI.Controllers;
 
-public class MainController {
+import javafx.fxml.FXML;
+import javafx.scene.layout.StackPane;
+import javafx.scene.Node;
+
+public class MainController
+{
+    @FXML
+    private StackPane contentArea;
+
+    @FXML private View1Controller view1Controller;
+    @FXML private View2Controller view2Controller;
+
+    @FXML private Node view1;
+    @FXML private Node view2;
+
+    @FXML public void initialize(){
+        view1.setVisible(false);
+        view1.setManaged(false);
+
+        view2.setVisible(false);
+        view2.setManaged(false);
+
+        showView(view1);
+    }
+
+    @FXML public void navigateToView(int view){
+        showView(view1);
+
+        System.out.println("Navegando a vista 1");
+    }
 }
