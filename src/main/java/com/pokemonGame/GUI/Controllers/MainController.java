78420d995baf8a670f1c5dd9d2bbe0cfac1063cf
Santiago Bonilla <112128287+ItzBoni@users.noreply.github.com>
2025-05-21 12:09:52 -0600

    Changed import routes and commented unused code.

--- src/main/java/com/pokemonGame/GUI/Controllers/MainController.java
+++ src/main/java/com/pokemonGame/GUI/Controllers/MainController.java
@@ -9,24 +9,24 @@ public class MainController
     @FXML
     private StackPane contentArea;
 
-    @FXML private View1Controller view1Controller;
-    @FXML private View2Controller view2Controller;
+    //@FXML private View1Controller view1Controller;
+    //@FXML private View2Controller view2Controller;
 
-    @FXML private Node view1;
-    @FXML private Node view2;
+    //@FXML private Node view1;
+    //@FXML private Node view2;
 
     @FXML public void initialize(){
-        view1.setVisible(false);
-        view1.setManaged(false);
+//        view1.setVisible(false);
+//        view1.setManaged(false);
+//
+//        view2.setVisible(false);
+//        view2.setManaged(false);
 
-        view2.setVisible(false);
-        view2.setManaged(false);
-
-        showView(view1);
+        //showView(view1);
     }
 
     @FXML public void navigateToView(int view){
-        showView(view1);
+        //showView(view1);
 
         System.out.println("Navegando a vista 1");
     }
