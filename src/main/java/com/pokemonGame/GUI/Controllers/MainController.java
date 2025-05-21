696ce9a89f0ec9254c4263dc6a5c6834d9e15474
Santiago Bonilla <112128287+ItzBoni@users.noreply.github.com>
2025-05-21 12:05:08 -0600

    Fixed controller issue

--- src/main/java/com/pokemonGame/GUI/Controllers/MainController.java
+++ src/main/java/com/pokemonGame/GUI/Controllers/MainController.java
@@ -1,45 +1,4 @@
 package com.pokemonGame.GUI.Controllers;
 
-import javafx.fxml.FXML;
-import javafx.scene.layout.StackPane;
-import javafx.scene.Node; 
-
 public class MainController {
-
-    @FXML
-    private StackPane contentArea;
-
-    // Controladores de las vistas que los inyectara en automatico es decir jalara en automatico los hijos 
-    // y puedes tener acceso a dicho controlador
-
-    // Por ejemplo podemos llamar a metodos setters de dicha vista si necesitamos acceder a propiedades del padre
-    @FXML private View1Controller view1Controller;
-    @FXML private View2Controller view2Controller;
-
-    // Este es el nodo en si, la vista.
-    @FXML private Node view1; 
-    @FXML private Node view2; 
-
-    @FXML
-    public void initialize() {
-        // Inicializa las vistas como no visibles y no manegables por lo que no 
-        // no se tomaran en cuenta tampoco para mostrar las vistas
-        view1.setVisible(false);
-        view1.setManaged(false); 
-
-        view2.setVisible(false);
-        view2.setManaged(false);}
-        // Muestra la vista correspondiente
-        showView(view1);
-    }
-
-    @FXML
-    public void navigateToView(int view) {
-        // Podemos hacer un ternario para que solo el de la vista deseada se muestra
-        
-        showView(view1);
-
-
-        System.out.println("Navegando a la Vista 1");
-    }
 }
